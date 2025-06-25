6bc1d3fe749d9922837936450fdc4c7e4e4a86a4
Drew ✦ Andersen <drew@monsterswithink.com>
2025-06-25 06:34:23 -0600

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,16 +1,21 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
 
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  const origin = url.origin;
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies })
-    await supabase.auth.exchangeCodeForSession(code)
-  }
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL('/dashboard', request.url))
+    if (!error && data.session) {
+      return NextResponse.redirect(`${origin}/dashboard`);
+    }
+  }
+  return NextResponse.redirect(`${origin}/dashboard?auth=error`);
 }
